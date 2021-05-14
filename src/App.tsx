6cd4cea3046c93799ec60b9e997cf851400a985d
BriWS <briws@pm.me>
2021-05-14 18:31:55 +0300

    03 - useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,33 @@
-import React, {useState} from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
 
   const [value, setValue] = useState(0)
 
-  const incHandler = () => {
-    setValue(value + 1)
-  }
-
-  const setToLocalStorageHandler = () => {
-    localStorage.setItem('counterValue', JSON.stringify(value))
-    localStorage.setItem('counterValue + 1', JSON.stringify(value + 1))
-  }
-
-  const getFromLocalHandler = () => {
+  // отработает единожды. Данные из localStorage будут получены только в том случае, когда приложение загрузится
+  useEffect(() => {
     let valueAsString = localStorage.getItem('counterValue')
     if (valueAsString) {
       let newValue = JSON.parse(valueAsString)
       setValue(newValue)
     }
-  }
+  }, [])
 
-  const clearLocalStorageHandler = () => {
-    localStorage.clear()
-    setValue(0)
-  }
+  // пападу каждый раз когда изменяется value. Будет устанавливаться новое значение в localStorage
+  useEffect(() => {
+    localStorage.setItem('counterValue', JSON.stringify(value))
+  }, [value])
 
-  const removeItemFromLocalStorageHandler = () => {
-    localStorage.removeItem('counterValue + 1')
+  const incHandler = () => {
+    setValue(value + 1)
   }
 
   return (
     <div className="App">
       <h1>{value}</h1>
       <button onClick={incHandler}>inc</button>
-      <button onClick={setToLocalStorageHandler}>setToLocalStorage</button>
-      <button onClick={getFromLocalHandler}>getFromLocalStorage</button>
-      <button onClick={clearLocalStorageHandler}>clearLocalStorage</button>
-      <button onClick={removeItemFromLocalStorageHandler}>removeItemFromLocalStorage</button>
+
     </div>
   );
 }
